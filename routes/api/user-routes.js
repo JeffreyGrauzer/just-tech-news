729c2627cf89f32bb77ad1bbb25fc36af98cd5d8
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-02-02 14:50:21 -0600

    added bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// GET /api/users
+// get all users
 router.get("/", (req, res) => {
-  //Access our User model and run .findAll() method)
   User.findAll({
     attributes: { exclude: ["password"] },
   })
@@ -14,7 +13,6 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
@@ -35,9 +33,8 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//POST /api/users
 router.post("/", (req, res) => {
-  // expects {username: 'Lernantino, email: 'lernantino@gmail.com', password: 'password1234'}
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -50,12 +47,12 @@ router.post("/", (req, res) => {
     });
 });
 
-// PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
@@ -72,6 +69,27 @@ router.put("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
 
 // DELETE /api/users/1
 router.delete("/:id", (req, res) => {
